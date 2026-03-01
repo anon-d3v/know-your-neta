8f6b58b7bf40fae56661d4e0d75cb1c443b32c86
anon-d3v <akashi3551@gmail.com>
2026-03-01 18:01:59 +0530

    Delete supabase directory

--- supabase/migrations/007_allow_nested_replies.sql
+++
@@ -1,3 +0,0 @@
--- Remove the 1-level nesting restriction to allow Reddit-style threaded discussions
-DROP TRIGGER IF EXISTS enforce_discussion_nesting ON public.discussions;
-DROP FUNCTION IF EXISTS public.check_discussion_nesting();
